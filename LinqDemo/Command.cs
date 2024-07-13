77fa818d64a8c88c2afa0c33e006ef6fa701042a
Nguyen van Phong <ngvanphong2012@gmail.com>
2024-07-13 16:40:40 +0700

    lllll

--- LinqDemo/Command.cs
+++ LinqDemo/Command.cs
@@ -18,91 +18,39 @@ namespace LinqDemo
             UIDocument uiDoc = commandData.Application.ActiveUIDocument;
             Document doc = uiDoc.Document;
 
-            IEnumerable<Element> columnCollection = new FilteredElementCollector(doc)
-                .OfCategory(BuiltInCategory.OST_StructuralColumns);
+            View activeView = doc.ActiveView;
 
-            Func<FamilyInstance, bool> function = (FamilyInstance a) =>
+            IEnumerable<Duct> filterCollection = new FilteredElementCollector(doc, activeView.Id).    // lay toan bo du lieu trong view hien tai dang thao tac
+                OfClass(typeof(Duct)).Cast<Duct>(); // chi lay cac doi truong ma co kieu du lieu class Duct va
+
+            List<Duct> listRoundDuct= new List<Duct>();
+            foreach (Duct item in filterCollection) 
             {
-                return a.Name == "W14x109";
-            };
+                Parameter parameter = item.get_Parameter(BuiltInParameter.RBS_CURVE_DIAMETER_PARAM);
+                if (parameter != null)
+                {
+                    listRoundDuct.Add(item);
+                }
+            }
+
+            var lisFilter = listRoundDuct.Where(x => x.Diameter < 0.3);
+
+            ICollection<Element> filterCollection2 = new FilteredElementCollector(doc)
+                .OfCategory(BuiltInCategory.OST_DuctCurves).WhereElementIsElementType().ToElements();
+
+
+            ElementCategoryFilter ductFilter= new ElementCategoryFilter(BuiltInCategory.OST_DuctCurves);
+            ElementCategoryFilter pipeFilter = new ElementCategoryFilter(BuiltInCategory.OST_PipeCurves);
+            ElementCategoryFilter fittingFiter = new ElementCategoryFilter(BuiltInCategory.OST_DuctFitting);
+            List<ElementFilter> listFilter= new List<ElementFilter> { ductFilter, pipeFilter,fittingFiter };
+            LogicalOrFilter orFilter = new LogicalOrFilter(listFilter);
+
+            LogicalAndFilter andFilter= new LogicalAndFilter(listFilter);
+
+            ICollection<Element> filterQuery = new FilteredElementCollector(doc,doc.ActiveView.Id).WherePasses(orFilter).ToElements();
             
-            List<Element> fabyNames= columnCollection.Where(x=>x.Name== "W14x109").ToList();
-
-            List<FamilyInstance> faOfType = columnCollection.OfType<FamilyInstance>().ToList();
-
-            List<FamilySymbol> listFamilySy= columnCollection.OfType<FamilySymbol>().ToList();
-
-            List<FamilySymbol> listOderByName= listFamilySy.OrderBy(y=>y.Name).ToList();
-            IEnumerable<FamilySymbol> listDesOrder = listFamilySy.OrderByDescending(y => y.Name);
-            IEnumerable<FamilySymbol> listOrder2 = listFamilySy.OrderBy(x => x.Name).ThenBy(y => y.Id);
-
-            IEnumerable<IGrouping<string,FamilySymbol>> groupName= listFamilySy.GroupBy(x => x.Name).ToList();
-
-            var group = from f in listFamilySy
-                        group f by f.Name;
-
-            IEnumerable<NameId> listSelect= listFamilySy.Select(x => new NameId(x.Name,x.Id));
-
-            var listSelect2 = from a in listFamilySy
-                              select new {a.Name, a.Id};
-
-            IEnumerable<string> listSelectName = listFamilySy.Select(x => x.Name);
-
-            bool isAllTrue = listFamilySy.All(x => x.Id.Value < 10000);
-
-            bool hasItem = listFamilySy.Any();
-            bool hasItemPrediction = listFamilySy.Any(x => x.Id.Value < 10000);
-
-            List<Element> listElement;
-            List<Element> listElement2 = new List<Element>();
-
-            FamilySymbol famyilyCheck = null;
-            bool isContaint = listFamilySy.Contains(famyilyCheck);
-
-            List<string> listName = new List<string> { "A", "B", "A", "C" };
-            bool hasC = listName.Contains("D");
 
 
-            int count1= listFamilySy.Count();
-
-            Func<FamilySymbol, bool> predictCount = (y) => { return y.Id.Value > 1000L;};
-
-            int count2 = listFamilySy.Count(x => x.Id.Value > 100L);
-
-            int count3 = listFamilySy.Count(predictCount);
-
-            // fa = listName[4];
-            //string fa= listName.ElementAt(4);
-
-            string fa = listName.ElementAtOrDefault(4);
-
-            string first = listName.First();
-            string last= listName.Last();
-
-            string first2 = listName.First(x => x == "A");
-
-            string name = "W14x109";
-            FamilySymbol faSyml = listFamilySy.First(x => x.Name == name);
-
-            FamilySymbol lastSyl= listFamilySy.Last(y=>y.Name == name);
-
-            FamilySymbol faSyl3= listFamilySy.FirstOrDefault(x => x.Name == name);
-            FamilySymbol faSy24= listFamilySy.LastOrDefault(y=>y.Name == name);
-
-            FamilySymbol fa5= listFamilySy.Single(x=>x.Name == name);
-            FamilySymbol fa6 = listFamilySy.SingleOrDefault(x => x.Name == name);
-
-            List<FamilySymbol> listFamilySy2= new List<FamilySymbol> { fa5,fa6 };
-
-            listFamilySy.Add(fa6);
-            listFamilySy.Add(fa5);
-            listFamilySy.Remove(fa6);
-
-            listFamilySy.AddRange(listFamilySy2);
-
-            var listConcat= listFamilySy.Concat(listFamilySy2);
-
-            var distinct= listFamilySy.Distinct();
 
 
 
