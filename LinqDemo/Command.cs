f97520a63643b99f6814c362aacad9100a83bdd3
Nguyen van Phong <ngvanphong2012@gmail.com>
2024-07-14 11:42:03 +0700

    52525525

--- LinqDemo/Command.cs
+++ LinqDemo/Command.cs
@@ -1,6 +1,7 @@
 ﻿using Autodesk.Revit.Attributes;
 using Autodesk.Revit.DB;
 using Autodesk.Revit.DB.Mechanical;
+using Autodesk.Revit.DB.Plumbing;
 using Autodesk.Revit.UI;
 using System;
 using System.Collections.Generic;
@@ -47,16 +48,71 @@ namespace LinqDemo
             LogicalAndFilter andFilter= new LogicalAndFilter(listFilter);
 
             ICollection<Element> filterQuery = new FilteredElementCollector(doc,doc.ActiveView.Id).WherePasses(orFilter).ToElements();
-            
-            
+
+
+            var col = new FilteredElementCollector(doc, doc.ActiveView.Id).OfCategory(BuiltInCategory.OST_DuctFitting).
+                WhereElementIsNotElementType().ToElements();
+
+            List<Element> listResult= new List<Element>();
+            foreach(Element item in col)
+            {
+                ElementId typeId = item.GetTypeId();
+                FamilySymbol familySymbol = doc.GetElement(typeId) as FamilySymbol;
+                if(familySymbol.Name=="1.5 D")
+                {
+                    listResult.Add(item);
+                }
+            }
+
+
+           
+            var collection2 = new FilteredElementCollector(doc, doc.ActiveView.Id).OfCategory(BuiltInCategory.OST_DuctFitting).
+                WhereElementIsNotElementType().Where(item =>
+                {
+                    ElementId typeId = item.GetTypeId();
+                    FamilySymbol familySymbol = doc.GetElement(typeId) as FamilySymbol;
+                    if (familySymbol.Name == "1.5 D") return true;
+                    else return false;
+
+                });
+
+
+
+            // lay tat ca system type 
+            // 
+
+            IEnumerable<PipingSystemType> pipipingSystemCollection = new FilteredElementCollector(doc) // lay toan bo piping system 
+                 .OfClass(typeof(PipingSystemType)).Cast<PipingSystemType>();
+            // chi lay system la sanitary
+            PipingSystemType santitarySystem = pipipingSystemCollection.First(x=>x.SystemClassification == MEPSystemClassification.Sanitary);
+
+            // lay tat ca cac pipi fitting trong view hien tai
+            var col2 = new FilteredElementCollector(doc, doc.ActiveView.Id).OfCategory(BuiltInCategory.OST_PipeFitting).
+               WhereElementIsNotElementType().ToElements();
+
+            List<Element> resutl2 = new List<Element>();
+            foreach (Element el in col2) // chay qua tung fitting
+            {
+                Parameter parameter = el.get_Parameter(BuiltInParameter.RBS_PIPING_SYSTEM_TYPE_PARAM); // truy cap parameter system type
+                ElementId systemId = parameter.AsElementId(); // lay gia tri cua parameter
+                if (systemId == santitarySystem.Id) // so sanh id
+                {
+                    resutl2.Add(el);
+                }
+
+
+            }
 
             
 
+            //       PipingSystemType santitarySystem = pipipingSystemCollection.First(x => x.Id.Value == 712047);
+
+            //       FamilyInstance pitting = null;
+            //       pitting.MEPModel.
 
 
 
-
-
+            uiDoc.Selection.SetElementIds(resutl2.Select(x => x.Id).ToList());
 
 
             return Result.Succeeded;
